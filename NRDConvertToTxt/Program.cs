5cde0c6c3d497c3163e4ac5f11cfa8629039e65d
AtomicJeff <45468813+AtomicJeff@users.noreply.github.com>
2019-12-27 05:36:27 -0800

    cleaned up code for readability

--- NRDConvertToTxt/Program.cs
+++ NRDConvertToTxt/Program.cs
@@ -22,8 +22,7 @@ namespace NRDConvertToTxt
             SqlCeDataReader sqlCeDataReader = new SqlCeCommand("SELECT * FROM MasterInstruments ORDER BY Name", connection).ExecuteReader();
             while (sqlCeDataReader.Read())
             {
-                int num = 20 - sqlCeDataReader[5].ToString().Length;
-                string str = new string(' ', num > 0 ? num : 20);
+                
                 lstMaster.Add(new MI()
                 {
                     Name = sqlCeDataReader[5].ToString(),
@@ -35,35 +34,42 @@ namespace NRDConvertToTxt
 
             string md = Environment.GetFolderPath(Environment.SpecialFolder.Personal) + $"\\NinjaTrader 8\\db\\replay\\{product}\\";
             var files = Directory.EnumerateFiles(md);
-            var t = 2;
+            
 
             foreach (var file in files)
             {
 
-                Console.WriteLine(file);
+                Console.WriteLine($"Converting file {file} from .nrd to .txt");
+
                 var name = Path.GetFileName(file);
                 string[] strArray = product.Split(' ');
                 string MIname = strArray[0];
+
                 MI mi = lstMaster.First<MI>((Func<MI, bool>)(i => i.Name == MIname));
                 MasterInstrument masterInstrument = new MasterInstrument();
                 masterInstrument.Name = mi.Name;
                 masterInstrument.InstrumentType = mi.Type;
                 masterInstrument.TickSize = mi.TickSize;
+
                 DateTime dateTime1 = new DateTime(2099, 12, 12);
                 if (strArray.Length == 2)
                 {
                     int int16 = (int)Convert.ToInt16(strArray[1].Split('-')[0]);
                     dateTime1 = new DateTime(2000 + (int)Convert.ToInt16(strArray[1].Split('-')[1]), int16, 15);
                 }
+
                 Instrument instrument = new Instrument()
                 {
                     Exchange = Exchange.Default,
                     Expiry = dateTime1,
                     MasterInstrument = masterInstrument
                 };
+
                 DateTime dateTime2 = new DateTime((int)Convert.ToInt16(name.Substring(0, 4)), (int)Convert.ToInt16(name.Substring(4, 2)), (int)Convert.ToInt16(name.Substring(6, 2)));
+
                 if (!Directory.Exists("CONVERTED\\" + product + "\\"))
                     Directory.CreateDirectory("CONVERTED\\" + product + "\\");
+
                 NinjaTrader.Data.MarketReplay.DumpMarketDepth(instrument, dateTime2, dateTime2.AddDays(1), "CONVERTED\\" + product + "\\" + name.Substring(0, 8) + ".txt");
 
 
